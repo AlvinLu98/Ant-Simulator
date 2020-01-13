4e11be8cf35982bd4de006140b6290935b23e17c
Alvin Lu <mmx15mxa@uea.ac.uk>
2020-01-13 12:53:46 +0000

    Removed intitial form screen

--- Ant_Simulator/src/Display.java
+++ Ant_Simulator/src/Display.java
@@ -1,8 +1,11 @@
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -31,22 +34,62 @@ public class Display extends Application {
     }
 
     //https://docs.oracle.com/javafx/2/get_started/form.htm
-    public void initialize(Stage formStage){
-        formStage.setTitle("Ant info");
-
-        createAntForm(formStage);
-
-        this.scene = new Scene(root, Simulator.WIDTH, Simulator.HEIGHT);
-        formStage.setScene(scene);
+    public void initialize(Stage menuStage){
+        createAntForm(menuStage);
     }
 
     private void createAntForm(Stage primaryStage){
+        Ant_Simulator ant = (Ant_Simulator)Display.sim;
+        ant.setInitAntAmt(500);
+        ant.setEvaporationRate(0.999);
+        ant.setNumFood(1);
+        ant.setObstacleType("Two Obstacle");
+        Ant_Simulator.setBirthRate(10);
+        Ant_Simulator.setLifespan(1);
+        Ant_Simulator.setScale(1);
+        setUpSimulation(primaryStage);
+    }
+
+    private void setUpSimulation(Stage primaryStage){
+        sim.initialize(primaryStage);
+        setUpMenu();
+
+        sim.buildLoop();
+
+        primaryStage.show();
+        menuStage.show();
+
+        ((Ant_Simulator)sim).setUp();
+    }
+
+    private void setUpMenu(){
+        //https://coderanch.com/t/620036/java/Stage-corner-screen
+        Rectangle2D screenSize = Screen.getPrimary().getBounds();
+
+        Display.menuStage = new Stage();
+        Display.menuStage.setTitle("Set up");
+        Display.menuStage.setX(screenSize.getMinX() + screenSize.getMaxX() - 700);
+        Display.menuStage.setY(screenSize.getMinY() + 20);
+
+        Scene menuScene;
+
         try {
-            this.root = FXMLLoader.load(getClass().getResource("Initial_Form.fxml"));
-            Initial_Form_Controller.setStage(primaryStage);
+            Parent menu = FXMLLoader.load(getClass().getResource("Map_Creation.fxml"));
+            menuScene = new Scene(menu, 500, 500);
+            createMenuEvents();
+            Display.menuStage.setScene(menuScene);
         }catch (IOException e){
-            System.out.println("Failed to load fxml file");
+            System.out.println("Failed to load set up fxml file");
         }
+    }
+
+    private void createMenuEvents(){
+        //https://www.programcreek.com/java-api-examples/?class=javafx.stage.Stage&method=setOnCloseRequest
+        Display.menuStage.setOnCloseRequest(event -> {
+            Platform.exit();
+        });
+
+        Display.menuStage.setResizable(false);
     }
 
     //https://www.geeksforgeeks.org/program-check-input-integer-string/
